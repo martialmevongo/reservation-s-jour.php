850ae3e753bb1fd5c6dfe0a44dd7eaec7ae2d3f5
martialmevongo <martial.mevongo@lapiscine.pro>
2025-04-14 14:28:40 +0200

    order

--- Reservation.model.php
+++ Reservation.model.php
@@ -21,6 +21,26 @@ public $bookedAt;
 public $cleaningOption;
 };
 
-$reservation = new reservation();
-// utilisateur envoie ces valeurs 
+
+$reservation = new Reservation();
+
+// utilisateur envoie ces valeurs
+// temporairement "en dur"
+$reservation->name = "David Robert";
+$reservation->place = "Château de Versailles";
+$reservation->startDate = new DateTime("25-04-15");
+$reservation->endDate = new DateTime("25-05-17");
+$reservation->cleaningOption = true;
+
+$reservation->nightPrice = 1000;
+
+// valeurs calculées automatiquement
+$totalPrice = (($reservation->endDate->getTimestamp() - $reservation->startDate->getTimestamp()) / (3600 * 24) * $reservation->nightPrice) + 5000;
+
+$reservation->totalPrice = $totalPrice;
+$reservation->bookedAt = new DateTime();
+$reservation->status = "CART";
+
+
+var_dump($reservation); 
 
