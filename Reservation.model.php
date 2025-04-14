9fce03fb769ea47c2d1b6be910b734fad52a495a
martialmevongo <martial.mevongo@lapiscine.pro>
2025-04-14 14:17:36 +0200

    public

---
+++ Reservation.model.php
@@ -0,0 +1,26 @@
+<?php
+
+class reservation{
+
+public $name
+
+public $place
+
+public $startDate
+
+public $endDate
+
+public $totalPrice
+
+public $nightPrice
+
+public $$status;
+
+public $bookedAt;
+
+public $cleaningOption;
+};
+
+$reservation = new reservation();
+// utilisateur envoie ces valeurs 
+
