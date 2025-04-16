caecd9a9cb26f993316805751397a6b58cac04af
martialmevongo <martial.mevongo@lapiscine.pro>
2025-04-16 10:16:18 +0200

    paid

---
+++ controller/cancel-reservation.controller.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../config.php');
+require_once('../model/Reservation.model.php');
+require_once('../model/Reservation.repository.php');
+
+// j'utilise la fonction findReservationForUser
+// pour récupérer la reservation créé par l'utilisateur (ou pas)
+// et je la stocke dans la variable $reservationForUser
+$reservationForUser = findReservationForUser();
+
+require_once('../view/cancel-reservation.view.php');
